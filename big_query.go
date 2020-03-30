54e1442c7ae54cff7d9b895e343f6190d13bee46
Nicole Gillett <nicole.gillett@cytora.com>
2020-03-30 14:12:07 +0100

    without query

--- big_query.go
+++ big_query.go
@@ -4,7 +4,6 @@ import (
 	"cloud.google.com/go/bigquery"
 	"context"
 	"fmt"
-	"google.golang.org/api/iterator"
 )
 
 type BQ struct {
@@ -21,41 +20,12 @@ func NewBigQueryClient(ctx context.Context) (*BQ, error) {
 	}, nil
 }
 
-func (bq *BQ) TableType(ctx context.Context) error {
-	q := bq.client.Query(`
-		SELECT DISTINCT metadata.proto_version
-    	FROM ` + "`cytora_dev_business_intelligence.properties`" + `
-	`)
-
-	it, err := q.Read(ctx)
+func (bq *BQ) Metadata(ctx context.Context) error {
+	md, err := bq.client.Dataset("cytora_dev_business_intelligence").Table("address_processing").Metadata(ctx)
 	if err != nil {
 		return err
 	}
-
-	err = iter(it)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (bq *BQ) TableVersion(ctx context.Context) error {
-	q := bq.client.Query(`
-		SELECT DISTINCT metadata.proto_version
-    	FROM ` + "`cytora_dev_business_intelligence.properties`" + `
-	`)
-
-	it, err := q.Read(ctx)
-	if err != nil {
-		return err
-	}
-
-	err = iter(it)
-	if err != nil {
-		return err
-	}
-
+	fmt.Println(md.ExternalDataConfig.SourceURIs)
 	return nil
 }
 
@@ -65,24 +35,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	//err = store.TableType(ctx)
-	err = store.TableVersion(ctx)
+	err = store.Metadata(ctx)
 	if err != nil {
 		panic(err)
 	}
-}
-
-func iter(it *bigquery.RowIterator) error {
-	for {
-		var value []bigquery.Value
-		err := it.Next(&value)
-		if err == iterator.Done {
-			break
-		}
-		if err != nil {
-			return err
-		}
-		fmt.Println(value)
-	}
-	return nil
 }
